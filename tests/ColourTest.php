3618581b551e390693b8197f84a2156ab74d6b08
Safak Samiloglu <safak.samiloglu@dreamapply.com>
2017-07-27 10:58:46 +0300

    Remove hhvm platform from Travis tests. Make extending class nightly complatible.

--- tests/ColourTest.php
+++ tests/ColourTest.php
@@ -2,7 +2,14 @@
 
 use Dream\Colour;
 
-class ColourTest extends PHPUnit_Framework_TestCase
+// Alias the PHPUnit 6.0 ancestor if available, else fall back to legacy ancestor
+if (class_exists('\PHPUnit\Framework\TestCase', true)) {
+    class ColourTestAncestor extends \PHPUnit\Framework\TestCase {}
+} else {
+    class ColourTestAncestor extends \PHPUnit_Framework_TestCase {}
+}
+
+class ColourTest extends ColourTestAncestor
 {
 	
     public function testWebValidation()
